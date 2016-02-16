047e9a09eed53c5ab027ebe87840940df9c70ce5
tyson <piotr_theis@o2.pl>
2016-02-16 09:37:11 +0100

    form fields factory

--- classes/jelly/form/core.php
+++ classes/jelly/form/core.php
@@ -1,4 +1,6 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');
+<?php
+
+defined('SYSPATH') or die('No direct access allowed.');
 
 /**
  * Jelly Form Core
@@ -12,34 +14,51 @@
  * @copyright  (c) 2016-2017 Piotr Theis
  * @license    http://www.opensource.org/licenses/isc-license.txt
  */
-abstract class Jelly_Form_Core{
-    
-    
-    
+abstract class Jelly_Form_Core
+{
+
     /**
-     * Model meta object
-     * @var \Jelly_Meta
+     * Form fields list
+     * @var array
      */
     protected $_fields;
 
-
+    /**
+     * Constructor
+     * 
+     * @param Jelly_Meta $meta
+     */
     public function __construct(Jelly_Meta $meta)
     {
         $this->_fields = $meta->fields();
     }
 
-    
-    
+    /**
+     * Gets or sets form fields
+     * 
+     * @param array $fields
+     * @return \Jelly_Form_Core|array
+     */
     public function fields(array $fields = null)
     {
+        if (!(bool) $fields)
+            return $this->_fields;
+
         $meta_fields = array_keys($this->_fields);
-        
+
         //unnecessary fields
-        foreach(array_diff($meta_fields, $fields) as $field)
+        foreach (array_diff($meta_fields, $fields) as $field)
         {
             unset($this->_fields[$field]);
         }
 
+        //convert model fields to form fields
+        foreach ($this->_fields as $key => $field)
+        {
+            $this->_fields[$key] = Jelly_Form_Field::factory($field);
+        }
+
         return $this;
-    }    
+    }
+
 }
---
+++ classes/jelly/form/field.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class Jelly_Form_Field extends Jelly_Form_Field_Core{}
